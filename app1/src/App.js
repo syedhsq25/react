a1c183e1335b1239528e03540b9be402b202cf9f
syedhsq25 <syedhsq25@gmail.com>
2024-02-13 21:10:37 -0800

    added header.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 function App() {
 
     return (
-        <center> <span  > <h1>hello</h1></span> <h1>world {1 + 1}</h1>
-        </center>)
+        <>
+            <center> <span  > <h1>hello</h1></span> <h1>world {1 + 1}</h1>
+            </center>
+        </>)
 }
 export default App;
---
+++ app1/src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cards(){
+    return(
+        <>
+        
+  <div class="column" style="background-color:#aaa;">Column</div>
+  </>
+
+
+    )
+}
