d49078543438b0a3ae276d8dbc97780e3710a4f3
syedhsq25 <syedhsq25@gmail.com>
2024-02-24 22:10:05 -0800

    2/24/2024

--- src/App.js
+++ src/App.js
@@ -1,55 +1,45 @@
 
 import './App.css';
+import axios from "axios"
+import { useState } from "react";
 
 function App() {
+  const getData = async () => {
 
-  const data = [
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "delectus aut autem",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "quis ut nam facilis et officia qui",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "fugiat veniam minus",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 4,
-      "title": "et porro tempora",
-      "completed": true
-    },
-    {
-      "userId": 1,
-      "id": 5,
-      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-      "completed": false
-    }]
+    try {
+
+      const res = await axios.get("https://jsonplaceholder.typicode.com/todos"
+      );
+      setData(res.data)
+
+    }
+    catch (error) {
+      console.log(console.error());
+    };
+  }
+
+
+  const [data, setData] = useState([]);
+
   return (
 
     <>
       <center>
-        <button>Task Status</button>
+        <button onClick={getData}>Load Data</button>
         <table><thead>
-          <th>users</th>
-          <th>id</th>
-          <th>title</th>
-          <th>completed</th></thead>
+          <tr>
+            <th >id</th>
+            <th>user id</th>
+            <th>title</th>
+            <th>task status</th></tr>
+        </thead>
           <tbody>
             {data.map(ele => (
 
-              <tr>
-                <td>{ele.userId}</td>
+              <tr key={ele.id}>
+
                 <td>{ele.id}</td>
+                <td>{ele.userId}</td>
                 <td>{ele.title}</td>
                 <td>{ele.completed ? "true" : "false"}</td>
               </tr>
