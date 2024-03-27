9defac230b7f6783ac5f3b03d5d7ff3021bef9b3
syedhsq25 <syedhsq25@gmail.com>
2024-03-27 01:23:29 -0700

    here in this app we tried more variables, objects , states etc

--- src/App.js
+++ src/App.js
@@ -3,57 +3,71 @@ import './App.css';
 import axios from "axios"
 import { useState, useEffect } from "react";
 function App() {
-
-
-  useEffect(() => {
-
-    const getData = async () => {
-
-
-      try {
-
-        const res = await axios.get("https://jsonplaceholder.typicode.com/todos"
-        );
-        setData(res.data)
-
-      }
-      catch (error) {
-        console.log(error);
-      };
-    }
-    getData()
-      , []
-  })
-
-
-
-
   const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setData(data);
 
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData();
+  }, []);
+  const extractAddress = (obj) => {
+    let arr = Object.values(obj);
+    let elems = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+      elems.push(<p key={i}>{arr[i]}</p>);
+    }
+    return elems;
+  }
   return (
 
     <><center>
-      <button >filter todos</button>
+
       <table><thead>
         <tr>
-          <th >id</th>
-          <th>user id</th>
-          <th>title</th>
-          <th>task status</th></tr>
+          <th rowSpan={2}>id</th>
+          <th rowSpan={2}>Name</th>
+          <th rowSpan={2}>Username</th>
+          <th rowSpan={2}>Email</th>
+          <th rowSpan={2}>Address</th>
+          <th colSpan={2}>Geo</th>
+          <th rowSpan={2}>Phone</th>
+          <th rowSpan={2}>Website</th>
+          <th rowSpan={2}>Company</th>
+        </tr>
+        <tr>
+          <th>Lat</th>
+          <th>Lon</th>
+
+        </tr>
       </thead>
         <tbody>
-          {data.map(ele => (
-
-            <tr key={ele.id}>
-
+          {data.map((ele, index) =>
+          (
+            <tr key={index}>
               <td>{ele.id}</td>
-              <td>{ele.userId}</td>
-              <td>{ele.title}</td>
-              <td>{ele.completed ? "false" : " "}</td>
+              <td>{ele.name}</td>
+              <td>{ele.username}</td>
+              <td>{ele.email}</td>
+              <td>{extractAddress(ele.address)}</td>
+              <td>{ele.address.geo.lat}</td>
+              <td>{ele.address.geo.lng}</td>
+              <td>{ele.phone}</td>
+              <td><a href={ele.website}>{ele.website}</a></td>
+              <td>{Object.values(ele.company).map((ele, i) => (<p key={i}> {ele} </p>))}</td>
             </tr>
+
           ))}
         </tbody>
-      </table></center >
+
+      </table>
+
+    </center >
     </>
   )
 }
